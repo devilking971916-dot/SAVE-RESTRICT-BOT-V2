235433ad7297de18765eb5c780dda640d9519d1d
anujedits42-byte <anujedits42@gmail.com>
2026-04-08 08:27:45 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -17,7 +17,7 @@ def health():
     return Response("OK", status=200)
 
 def _run():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(
         host="0.0.0.0",
         port=port,
